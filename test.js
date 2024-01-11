c62dd7717aadb33545e0ac3b6a98c6c042e8ce37
elyHristova <elynka.georgieva@gmail.com>
2024-01-11 21:58:20 +0200

    1000 Create function sum

---
+++ test.js
@@ -0,0 +1,17 @@
+// // a + b result
+// function sum (a, b) {
+//     return a + b;
+// }
+
+
+
+
+
+function addOne (arg) {
+    return arg + 1
+}
+function sum2 (a, b, addOne) {
+    return (addOne (a + b));
+}
+const result = sum2 (2, 2, addOne)
+console.log (`After ${result}`)
